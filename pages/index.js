e26cb4cb044bd36db71dd1c0298bdfb9c4d62036
VITALIY1986 <staslego1986@gmail.com>
2020-11-17 23:51:18 +0200

    bbb

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,49 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div class='container my-0 mx-auto'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
        
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Larn to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      
+        <header>
+          <div class="flex bg-gray-200 justify-between">
+            <a class="p-4">Logo</a>
+            <div class="p-4">burger</div>
+          </div>
+        </header>
+        <div class="h-screen">
+        <img
+          class='object-cover h-screen w-full'
+          src="/www.jpg" alt="Vercel Logo"
+        />
         </div>
-      </main>
+        <div><Link href="/posts/first-post"><a>this page!</a></Link></div>
+   
+        
+        <div className='md:flex bg-white rounded-lg p-24 '>
+        
+        <img
+          className='h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6'
+          src="/www.jpg" alt="Vercel Logo"
+        />
+        
+        <div className='text-center md:text-left'>
+          <h2 className='Sblack'>Jake Prins</h2>
+          <div className='text-purple-500'>JavaScript developer</div>
+          <div className='text-purple-500'>Twitter: @jakeprins_nl</div>
+        <div className='text-gray-600'>www.jakeprins.com</div>
+      </div>
+    </div>
+     
+     
+     
      
       <footer className={styles.footer}>
         <a
@@ -61,6 +55,7 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
+        
     </div>
   )
 }

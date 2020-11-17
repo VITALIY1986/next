1ae5dc123fa17761c2e224277c8a37749fcccdc3
VITALIY1986 <staslego1986@gmail.com>
2020-11-18 00:10:58 +0200

    fsdg

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div class='container my-0 mx-auto'>
